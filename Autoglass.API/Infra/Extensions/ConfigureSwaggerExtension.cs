8dca7efc450ee2cd40a898a1489af936f8e116b1
Eduardo Sabino <eduardo.lopes-ext@telecall.com>
2024-03-25 23:20:18 -0300

    Configurando swagger

---
+++ Autoglass.API/Infra/Extensions/ConfigureSwaggerExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Oceanica.GupyProd.Extensions;
+
+public static class ConfigureSwaggerExtension
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Autoglass API",
+                Version = "v1",
+                Description = "Autoglass API Services.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Eduardo Sabino"
+                },
+            });
+
+            c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+    }
+
+    public static void UseSwaggerCustomize(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Autoglass API");
+            c.RoutePrefix = string.Empty;
+        });
+    }
+}
--- Autoglass.API/Program.cs
+++ Autoglass.API/Program.cs
@@ -1,24 +1,28 @@
 using Autoglass.API.Infra.Configurations;
 using Autoglass.API.Infra.Extensions;
 using Autoglass.API.Infra.Repositories;
+using Autoglass.API.Services;
+using Oceanica.GupyProd.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 // Add services to the container.
 
 //Configurations
+services.AddEndpointsApiExplorer();
 services.ConfigureDbConfigOptions();
 services.ConfigureDbContext();
 services.ConfigureDbHealthCheck();
 services.ConfigureAutomapper();
-
+services.ConfigureBaseUrl();
+services.ConfigureSwagger();
 services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
 
 //Binding services
+services.AddHttpContextAccessor();
 services.AddScoped<IProductRepository, ProductRepository>();
+services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
@@ -32,6 +36,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSwaggerCustomize();
 
 app.UseHealthChecks("/status");
 
